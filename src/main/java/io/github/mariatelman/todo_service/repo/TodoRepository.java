27110ff81ad50f5052ea13be9a0ebd743cbd58ee
mgorpenko <mgorpenko@outbrain.com>
2026-03-01 18:15:42 +0200

    adding a repository

---
+++ src/main/java/io/github/mariatelman/todo_service/repo/TodoRepository.java
@@ -0,0 +1,19 @@
+package io.github.mariatelman.todo_service.repo;
+
+
+import io.github.mariatelman.todo_service.domain.model.TodoStatus;
+import io.github.mariatelman.todo_service.domain.model.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<TodoItem, Long> {
+
+  List<TodoItem> findAllByOrderByCreatedAtAsc();
+
+  @Query("SELECT t FROM TodoItem t WHERE t.status = 'NOT_DONE' AND (t.dueAt is NULL or t.dueAt > :now) ORDER BY t.createdAt ASC")
+  List<TodoItem> findAllNotDoneItems(@Param("now") Instant now);
+}
